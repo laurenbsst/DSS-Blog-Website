8e2e63ed0ce7442a3b389b364a4a2dc70dc39eae
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-03-12 17:51:36 +0000

    Finished basic set up

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('login');
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running on port 5000');
+});
