2e414dba70cb4b2f83cf99d665ae3228a6263922
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-03-20 16:27:33 +0000

    Created connection to local PostgreSQL database

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDb = async () => {
+    try {
+        const pool = new Pool({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT,
+        });
+        await pool.connect();
+        const res = await pool.query('SELECT * FROM users');
+        console.log(res);
+        console.log("Successfully connected to the database!");
+        await pool.end();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDb()
+
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
