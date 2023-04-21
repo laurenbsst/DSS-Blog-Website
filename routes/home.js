f43a34bd66ddb45db02f108fa14ba3eca4f142a2
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-04-21 18:36:13 +0100

    Show posts from user when given id

--- routes/home.js
+++ routes/home.js
@@ -2,14 +2,21 @@ const express = require('express');
 const homeRouter = express();
 const db = require('../db/index');
 
-homeRouter.get('/', (req, res, next) => {
-    db.query('SELECT * FROM users WHERE user_id = 1', (err, result) => {
-        if (err) {
-          return next(err)
-        }
-        console.log(result.rows[0]);
-        res.render('home', {users: result.rows});
-      })
+homeRouter.get('/:user_id', (req, res, next) => {
+  const user_id = req.params.user_id;
+  db.query(`SELECT * FROM users WHERE user_id = $1`, [user_id], (err, user_result) => {
+    if (err) {
+      return next(err)
+    }
+  
+  db.query(`SELECT * FROM posts WHERE user_id = $1`, [user_id], (err, post_result) => {
+    if (err) {
+      return next(err)
+    }
+   
+  res.render('home', {users: user_result.rows, posts: post_result.rows});
+});
+});
 });
 
 module.exports = homeRouter;
