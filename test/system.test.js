e81113ee8d3d413e27488ea2768f2a5442584d48
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-05-16 21:36:35 +0100

    Updated test ids

--- test/system.test.js
+++ test/system.test.js
@@ -10,7 +10,7 @@ describe('Home routes', () => {
     // GET requests
     it('Should render home screen with user posts', (done) => {
         chai.request(server)
-        .get('/home/6')
+        .get('/home/1f2768b7-d50f-4141-ba5c-b388413e9b44')
         .end((err, res) => {
             res.should.have.status(200);
             expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
@@ -20,7 +20,7 @@ describe('Home routes', () => {
     })
     it('Should render new post screen', (done) => {
         chai.request(server)
-        .get('/home/6/new-post')
+        .get('/home/1f2768b7-d50f-4141-ba5c-b388413e9b44/new-post')
         .end((err, res) => {
             res.should.have.status(200);
             expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
@@ -30,7 +30,7 @@ describe('Home routes', () => {
     })
     it('Should render view post screen with post content populated', (done) => {
         chai.request(server)
-        .get('/home/6/1/view')
+        .get('/home/1f2768b7-d50f-4141-ba5c-b388413e9b44/e98b35d1-e3b5-42e7-908c-fd17ef88a11e/view')
         .end((err, res) => {
             res.should.have.status(200);
             expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
@@ -47,7 +47,7 @@ describe('Home routes', () => {
         }
 
         chai.request(server)
-        .post('/home/6/new-post/submit')
+        .post('/home/1f2768b7-d50f-4141-ba5c-b388413e9b44/new-post/submit')
         .send(blog_post)
         .end((err, res) => {
             res.should.have.status(200);
@@ -56,4 +56,21 @@ describe('Home routes', () => {
         done();
         })
     })
+    //it('Should create a new account', (done) => {
+        //let account = {
+           // username: 'admin',
+            //email: '1234@gmail.com',
+            //password: '1234',
+            //confirmpassword: '1234'
+        //}
+        //chai.request(server)
+        //.post('/create-account')
+        //.send(account)
+        //.end((err, res) => {
+            //res.should.have.status(500);
+            //expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
+           // res.body.should.be.a('object');
+        //done();
+        //})
+    //})
 })
